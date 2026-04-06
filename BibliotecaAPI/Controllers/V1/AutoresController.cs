1e408330fdcf8b7bf0a59c5bb02e8975c74cf811
ctobler <ctobler87@gmail.com>
2026-04-06 18:07:34 +0100

    Remove exception to enable Get method functionality
    Removed an exception throw statement to allow normal execution.

--- BibliotecaAPI/Controllers/V1/AutoresController.cs
+++ BibliotecaAPI/Controllers/V1/AutoresController.cs
@@ -48,8 +48,7 @@ namespace BibliotecaAPI.Controllers.V1
         [ServiceFilter<HATEOASAutoresAttribute>()]
         public async Task<IEnumerable<AutorDTO>> 
             Get([FromQuery] PaginacionDTO paginacionDTO)
-        {
-            throw new ApplicationException("Debes remover esta linea");
+        {            
             return await servicioAutoresV1.Get(paginacionDTO);
         } 
 
